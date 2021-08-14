9c1fae91f880c227f9ab48d3aee5ebfb20716591
Suemai <69875258+Suemai@users.noreply.github.com>
2021-08-14 03:45:46 +0100

    Added Stuff
    Added:
    dishes recycler view adaptor, later to be changed to scroll view maybe?
    added text button layout for each dish - needs to be prettied up
    
    Changed:
    layout of search bar and recycler view changed a bit

--- Menu_app/app/src/main/java/com/example/menu_app/MainActivity.java
+++ Menu_app/app/src/main/java/com/example/menu_app/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.menu_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView dishesRecView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dishesRecView = findViewById(R.id.dishesRecView);
+
+        ArrayList<dish> dishes = new ArrayList<>();
+        dishes.add(new dish("Barbecued Spare Ribs with BBQ Sauce", "骨", "骨", "1", 0.00, 6.50, 0));
+        dishes.add(new dish("Spare Ribs with Chilli and Salt", "椒盐骨", "盐骨", "2", 0.00, 6.50, 0));
+        dishes.add(new dish("Barbecued Spare Ribs with Honey Sauce", "蜜骨", "蜜骨", "3", 0.00, 6.50, 0));
+        dishes.add(new dish("Sweet & Sour Spare Ribs", "咕咾骨", "古骨", "4", 0.00, 6.50, 0));
+        dishes.add(new dish("Chicken Wings with Chilli & Salt", "椒盐鸡翅", "盐鸡羽", "8", 0.00, 6.50, 0));
+        dishes.add(new dish("Crab Claws (4)", "蟹爪", "蟹爪", "21", 3.60, 0.00, 0));
+        dishes.add(new dish("Crispy Aromatic Duck", "香鸭", "香甲", "28", 8.80, 16.30, 0));
+
+        dishesRecViewAdapter adapter = new dishesRecViewAdapter(this);
+        adapter.setDishes(dishes);
+
+        dishesRecView.setAdapter(adapter);
+        dishesRecView.setLayoutManager(new LinearLayoutManager(this));
+
     }
 }
---
+++ Menu_app/app/src/main/java/com/example/menu_app/dishesRecViewAdapter.java
@@ -0,0 +1,67 @@
+package com.example.menu_app;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class dishesRecViewAdapter extends RecyclerView.Adapter<dishesRecViewAdapter.ViewHolder>{
+
+    private ArrayList<dish> dishes = new ArrayList<dish>();
+    private Context context;
+
+    //constructor
+    public dishesRecViewAdapter(Context context){
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.dishes_list_item, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, @SuppressLint("RecyclerView") final int position) {
+        holder.txtDishes.setText(dishes.get(position).getName());
+        holder.parent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, dishes.get(position).getName() + " selected", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return dishes.size();
+    }
+
+    public void setDishes(ArrayList<dish> dishes){
+        this.dishes = dishes;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        private TextView txtDishes;
+        private RelativeLayout parent;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            txtDishes = itemView.findViewById(R.id.txtDishes);
+            parent = itemView.findViewById(R.id.parent);
+        }
+    }
+}
